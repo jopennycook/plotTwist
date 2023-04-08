f3a683e00b23b420a0e24f563ceff712b2d6e919
jopennycook <joe.pennycook@hotmail.co.uk>
2023-04-08 12:04:04 +0100

    temporarily disabling nunito

--- R/simple_theme.R
+++ R/simple_theme.R
@@ -7,20 +7,20 @@
 #'
 #' @examples
 simple_theme <- function() {
-        theme(axis.title.x = element_markdown(family = "Nunito500", size = 12),
-              axis.title.y = element_markdown(family = "Nunito500", size = 12),
-              axis.text.x = element_markdown(family = "Nunito700", size = 9, colour = "black"),
-              axis.text.y = element_markdown(family = "Nunito700", size = 9, colour = "black"),
+        theme(axis.title.x = element_markdown(family = "sans", size = 12),
+              axis.title.y = element_markdown(family = "sans", size = 12),
+              axis.text.x = element_markdown(family = "sans", size = 9, colour = "black"),
+              axis.text.y = element_markdown(family = "sans", size = 9, colour = "black"),
               axis.line.x = element_blank(),
               axis.line.y = element_blank(),
               axis.ticks = element_line(linewidth = 0.5, colour = "black"),
-              strip.text.x = element_markdown(family = "Nunito700", size = 9, colour = "black"),
-              strip.text.y = element_markdown(family = "Nunito700", size = 9, colour = "black"),
+              strip.text.x = element_markdown(family = "sans", size = 9, colour = "black"),
+              strip.text.y = element_markdown(family = "sans", size = 9, colour = "black"),
               strip.background = element_blank(),
               panel.grid = element_blank(), panel.border = element_blank(),
               panel.background = element_rect(fill = "white", colour = "black", linewidth = 1),
               legend.background = element_blank(), legend.key = element_blank(),
-              legend.title = element_markdown(family = "Nunito500", size = 12),
+              legend.title = element_markdown(family = "sans", size = 12),
               legend.title.align = 0.5,
-              legend.text = element_markdown(family = "Nunito700", size = 9, colour = "black"))
+              legend.text = element_markdown(family = "sans", size = 9, colour = "black"))
 }
--- R/zzz.R
+++ R/zzz.R
@@ -8,18 +8,18 @@
 #' @import sysfonts
 #'
 #' @examples
-.onLoad <- function(libname, pkgname) {
+#.onLoad <- function(libname, pkgname) {
 #        font_add_google("Nunito", family = "Nunito200", regular.wt = 200)
 #        font_add_google("Nunito", family = "Nunito300", regular.wt = 300)
 #        font_add_google("Nunito", family = "Nunito400", regular.wt = 400)
-        font_add_google("Nunito", family = "Nunito500", regular.wt = 500)
+#        font_add_google("Nunito", family = "Nunito500", regular.wt = 500)
 #        font_add_google("Nunito", family = "Nunito600", regular.wt = 600)
-        font_add_google("Nunito", family = "Nunito700", regular.wt = 700)
+#        font_add_google("Nunito", family = "Nunito700", regular.wt = 700)
 #        font_add_google("Nunito", family = "Nunito800", regular.wt = 800)
 
-        showtext_auto(TRUE)
-}
+#        showtext_auto(TRUE)
+#}
 
-.onUnload <- function(libname, pkgname) {
-        showtext_auto(FALSE)
-}
+#.onUnload <- function(libname, pkgname) {
+#        showtext_auto(FALSE)
+#}
